4fa503ac52239bb21935b89fc9b3f050657fd908
Auckloo Veevek <avish@Auckloos-MacBook-Pro.local>
2024-11-15 00:00:25 -0500

    Added form structure in index.html and added form validation in script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('myForm');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+
+const nameError = document.getElementById('nameError');
+const emailError = document.getElementById('emailError');
+
+const button = document.getElementById('button');
+const details = document.getElementById('details');
+
+button.addEventListener('click', () => {
+    details.style.display("block");
+})
+
+form.addEventListener('submit', function(event) {
+    nameError.textContent = '';
+
+    if (nameInput.value.trim() === '') {
+        nameError.textContent = 'Please Enter Your Name';
+        event.preventDefault();
+    }
+
+    if (emailInput.value.trim() == ''){
+        emailError.textContent = "Please Enter Your Email.";
+        event.preventDefault();
+    }  else if (!validateEmail(emailInput.value)) {
+        emailError.textContent = 'Please Enter a Valid Email Address';
+        event.preventDefault();}
+    
+
+
+});
+
+function validateEmail(email) {
+    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+return re.test(String(email).toLowerCase());
+}
